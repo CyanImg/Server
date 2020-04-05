e263d158dd5fb344dab2ae84cb35c7a8943380fe
icbmicbm <icbmicbm@yandex.com>
2020-04-05 10:35:03 +0800

    first version of getPic to set icon

--- server/settings.py
+++ server/settings.py
@@ -14,6 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MEDIA_ROOT = "/Users/icbm/Desktop/Server/media"
+MEDIA_URL = "media/"
 
 
 # Quick-start development settings - unsuitable for production
@@ -46,7 +48,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
